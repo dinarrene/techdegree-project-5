f5df1089f3cf483468128b420aaf130c39769069
dinarrene <dinardavis@gmail.com>
2020-10-03 22:22:46 -0700

    add card EventListener

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,23 @@ let employeeData = [];
 
 function fetchData(url) {
   return fetch(url)
-  .then(res => res.json())
-  .catch(error => console.log('Error Fetching Data:', error))    
+    .then(res => res.json())
+    .catch(error => console.log('Error Fetching Data:', error))    
 }
 
 fetchData(randEmployees)
   .then(data => {
-      data.results.map(result => employeeData.push(result))
-      generateCard(employeeData)
+    data.results.map(result => employeeData.push(result))
+    generateCard(employeeData)  
+
+    const cards = document.querySelectorAll('.card');
+    const cardsArray = Array.from(cards);
+
+    cards.forEach(card => {
+      card.addEventListener('click', (e) => {
+        console.log(cardsArray.indexOf(e.target));
+      })
+    })
   });
 
 
@@ -20,20 +29,10 @@ fetchData(randEmployees)
 //  HELPER FUNCTIONS
 // ------------------------------------------
 
-function checkStatus(response) {
-  if (response.ok) {
-      return Promise.resolve(response);
-  } else {
-      return Promise.reject(new Error(response.statusText));
-  }
-}
-
 function generateCard(data) {
   data.map( employee => {
-    const card = document.createElement('div');
-    gallery.appendChild(card);
-    card.setAttribute('class', 'card')
-    card.innerHTML = ` 
+    const html = ` 
+      <div class="card">
         <div class="card-img-container">
             <img class="card-img" src="${employee.picture.large}" alt="profile picture">
         </div>
@@ -42,7 +41,37 @@ function generateCard(data) {
             <p class="card-text">${employee.email}</p>
             <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
         </div>
+      </div>
     `;
-  });
+    gallery.insertAdjacentHTML('beforeend', html)
+  }); 
+}
+
+
+
+
+
+
+function generateModal() {
+  // <div class="modal-container">
+  //     <div class="modal">
+  //         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+  //         <div class="modal-info-container">
+  //             <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+  //             <h3 id="name" class="modal-name cap">name</h3>
+  //             <p class="modal-text">email</p>
+  //             <p class="modal-text cap">city</p>
+  //             <hr>
+  //             <p class="modal-text">(555) 555-5555</p>
+  //             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+  //             <p class="modal-text">Birthday: 10/21/2015</p>
+  //         </div>
+  //     </div>
+
+  //     <div class="modal-btn-container">
+  //         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+  //         <button type="button" id="modal-next" class="modal-next btn">Next</button>
+  //     </div>
+  // </div>
 }
 
